76b19b36ba550e4c60e359d7542bd63dbe560a8f
RagavN12 <Born@mdu12>
2026-07-18 15:06:07 -0400

    initial startup

---
+++ EcoAI/EcoAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoAIApp.swift
+//  EcoAI
+//
+//  Created by Ragav Naresh on 7/18/26.
+//
+
+import SwiftUI
+
+@main
+struct EcoAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
